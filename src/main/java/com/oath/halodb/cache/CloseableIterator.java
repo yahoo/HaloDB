58797849dc9e30cb599f999045cd4565f7e0db79
Arjun Mannaly <amannaly@yahoo-inc.com>
2018-06-08 11:08:55 -0700

    Add notice file and fix licenses.

--- src/main/java/com/oath/halodb/cache/CloseableIterator.java
+++ src/main/java/com/oath/halodb/cache/CloseableIterator.java
@@ -3,6 +3,8 @@
  * Licensed under the terms of the Apache License 2.0. Please refer to accompanying LICENSE file for terms.
  */
 
+// This code is a derivative work heavily modified from the OHC project. See NOTICE file for copyright and license.
+
 package com.oath.halodb.cache;
 
 import java.io.Closeable;
--- src/main/java/com/oath/halodb/cache/histo/EstimatedHistogram.java
+++ src/main/java/com/oath/halodb/cache/histo/EstimatedHistogram.java
@@ -3,23 +3,8 @@
  * Licensed under the terms of the Apache License 2.0. Please refer to accompanying LICENSE file for terms.
  */
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// This code is a derivative work heavily modified from the OHC project. See NOTICE file for copyright and license.
+
 package com.oath.halodb.cache.histo;
 
 import com.google.common.base.Objects;
